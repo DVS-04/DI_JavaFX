0671a29c4c565d10660d5a811c509e6d0ce532a6
David Vega <davidvegas240@gmail.com>
2024-10-21 12:58:40 +0200

    Add files via upload

---
+++ ejFXML_DavidVega/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module es.ieslosmontecillos.ejfxml_davidvega {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens es.ieslosmontecillos.ejfxml_davidvega to javafx.fxml;
+    exports es.ieslosmontecillos.ejfxml_davidvega;
+}
